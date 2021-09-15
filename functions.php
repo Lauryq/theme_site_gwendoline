5ded3df3fd669b94c92974659f38490b373184b5
“laury <lquarto.lq@gmail.com>
2021-09-15 15:47:17 +0200

    ajout structure dossier + header + footer

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+// Lier le style.css
+wp_enqueue_style( 
+    'style.css',
+    get_stylesheet_uri(), 
+    array(), 
+    '1.0'
+);
+
+// Lier le JS
+wp_enqueue_script( 
+    'js/script.js',
+);
