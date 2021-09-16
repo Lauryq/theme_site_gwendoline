ada6e055c120898f934cad8aab7cd048b77197f7
“laury <lquarto.lq@gmail.com>
2021-09-16 16:58:31 +0200

    loop pour section devenir_sponso

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 <main>
 <!-- section description -->
+<section id="descritption">
+
 <?php $loop = new WP_Query((array('post_type' => 'description',))); ?>
 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
         <h2><?php the_title() ?></h2>
@@ -9,7 +11,10 @@
 <?php endwhile;
 wp_reset_query(); ?>
 
+</section>
+
 <!-- section parcours -->
+<section id="parcours">
 
 <?php $loop = new WP_Query((array('post_type' => 'parcours',))); ?>
 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
@@ -17,6 +22,8 @@ wp_reset_query(); ?>
 <?php the_content() ?>
 <?php endwhile;
         wp_reset_query(); ?>
+
+</section>
 
 
 <!-- section actualites -->
@@ -32,12 +39,28 @@ wp_reset_query(); ?>
 </section>
 
 <!-- section sponsors -->
+<section id="sponsors">
+
 <?php $loop = new WP_Query((array('post_type' => 'sponsors',))); ?>
 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
         <h2><?php the_title() ?></h2>
         <?php the_content() ?>
 <?php endwhile;
 wp_reset_query(); ?>
+
+</section>
+
+<!-- section devenirsponsors -->
+<section id="devenir_sponso">
+
+<?php $loop = new WP_Query((array('post_type' => 'devenir_sponso',))); ?>
+<?php while ($loop->have_posts()) : $loop->the_post(); ?>
+        <h2><?php the_title() ?></h2>
+        <?php the_content() ?>
+<?php endwhile;
+wp_reset_query(); ?>
+
+</section>
 
 
 </main>
