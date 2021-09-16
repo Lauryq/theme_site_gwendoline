59e3436787dd6448577f3c99ddca49e106389a50
“laury <lquarto.lq@gmail.com>
2021-09-16 15:46:04 +0200

    ajout main dans index + dans le style

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
-<?php $loop = new WP_Query((array('post_type' => 'parcours',))); ?>
-        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                <h1 class="coucou"><?php the_title() ?></h1><br>
-                <?php the_content() ?>
-            <?php endwhile; wp_reset_query(); ?>
+<main>
 
+        <?php $loop = new WP_Query((array('post_type' => 'parcours'))); ?>
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+        <h1 class="coucou"><?php the_title() ?></h1><br>
+        <?php the_content() ?>
+        <?php endwhile; wp_reset_query(); ?>
+        
+</main>
 <?php get_footer(); ?>
